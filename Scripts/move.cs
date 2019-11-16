d43e63f5229e08d35da6b3e57c0001355963fec1
Yashank18 <yashank1050.cse18@chitkara.edu.in>
2019-11-16 16:57:24 +0530

    Added a Script
    Initial Trial.

---
+++ Scripts/move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public Transform[] target;
+   public float speed;
+  
+   private int current;
+
+
+
+   void Update () {
+   
+    
+    
+    if (transform.position != target[current].position)
+    {
+      Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+      GetComponent<Rigidbody>().MovePosition(pos);
+    this.transform.LookAt(target[current].position);
+    
+    
+
+
+    }
+    else {
+        if(current == (target.Length))
+        {
+          return;
+        }
+        else
+      current=(current+2);
+  
+         
+      }
+    }
+    
+   
+}
